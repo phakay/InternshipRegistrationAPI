d3563a60b62c84d1b6e223b2e04859a9ec1d55c0
farouk subair <farouk_subair@yahoo.com>
2023-10-13 11:49:47 +0100

    add model for ApplicationForm

---
+++ InternshipRegistrationAPI.Core/Models/ApplicationForm.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using InternshipRegistrationAPI.Core.Contracts;
+using Newtonsoft.Json;
+
+namespace InternshipRegistrationAPI.Core.Models;
+
+public class ApplicationForm : IDistributableEntity
+{
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+    public string UploadCoverImageUrl { get; set; }
+    public string Type { get; set; }
+    public PersonalInformation PersonalInformation { get; set; }
+    public Profile Profile { get; set; }
+    
+    
+    
+    #region IDistributable Property
+    public string PartitionKey => Type;
+    #endregion
+}
+
+public class PersonalInformation
+{
+    public PersonalInformationTemplate FirstName { get; set; }
+    public PersonalInformationTemplate LastName { get; set; }
+    public PersonalInformationTemplate Email { get; set; }
+    public PersonalInformationTemplate PhoneNumber { get; set; }
+    public PersonalInformationTemplate Nationality { get; set; }
+    public PersonalInformationTemplate CurrentResidence { get; set; }
+    public PersonalInformationTemplate IdNumber { get; set; }
+    public PersonalInformationTemplate DateOfBirth { get; set; }
+    public PersonalInformationTemplate Gender { get; set; }
+    public List<QuestionTemplate> PersonalQuestions { get; set; }
+    public List<QuestionTemplate> AdditionalQuestions { get; set; }
+}
+
+public class PersonalInformationTemplate
+{
+    public bool IsInternal { get; set; }
+    public bool Hidden { get; set; }
+}
+
+public class Profile
+{
+    public ProfileTemplate Education { get; set; }
+    public ProfileTemplate Experience { get; set; }
+    public ProfileTemplate Resume { get; set; }
+    public List<QuestionTemplate> ProfileQuestions { get; set; }
+}
+public class ProfileTemplate
+{
+    public bool IsMandatory { get; set; }
+    public bool Hidden { get; set; }
+}
+
+public class QuestionTemplate
+{
+    public string Id { get; set; }
+    public QuestionType TypeOfQuestion { get; set;  }
+    public string Question { get; set; }
+    public List<string> Choices { get; set; }
+}
+
+public enum QuestionType
+{
+    Paragraph,
+    Dropdown,
+    YesNo,
+    MultipleChoice
+}
